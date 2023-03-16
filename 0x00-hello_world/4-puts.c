4f4d135fde0d2ac549cb636cf841ed476b4150d0
HichamZineb <hichamelamine@hotmail.com>
2023-03-16 09:14:20 -0700

    update6 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - prints string
+ */
 int main(void)
-/* main prints string */
+
 {
 	puts ("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return(0);
 
 }
