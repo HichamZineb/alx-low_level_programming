8b2e864e3ac96721a64181fb05e5de509a205806
HichamZineb <hichamelamine@hotmail.com>
2023-03-16 09:35:12 -0700

    newfile 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints string
+ *
+ * Return: returns 0 if everything is ok
+ */
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+
+}
