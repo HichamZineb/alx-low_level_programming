67084ab13ac4568540aae54c6f6d7b285d77a3d3
HichamZineb <hichamelamine@hotmail.com>
2023-03-16 09:24:02 -0700

    update7 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,11 @@
 int main(void)
 
 {
-	puts ("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 
 }
+
+/*
+ * return - returns 0 if everything is ok
+ */
