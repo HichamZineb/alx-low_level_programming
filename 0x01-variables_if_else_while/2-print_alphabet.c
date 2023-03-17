e508cc50f28b09f67cb4fc359c1c8a135a9a973a
HichamZineb <hichamelamine@hotmail.com>
2023-03-17 08:38:48 -0700

    newfile 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - prints if random number is negative, positive or is zero
+ *
+ * Return: returns 0 if everything is ok
+ */
+
+int main(void)
+
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+	putchar(ch);
+	ch++;
+	}
+	return (0);
+}
