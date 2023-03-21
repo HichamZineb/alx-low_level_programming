e8db9f9f32169dc6ea1255b3eb7d8f6b52a1cb88
HichamZineb <hichamelamine@hotmail.com>
2023-03-21 06:53:46 -0700

    update1 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,14 @@
-#include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
  * main - prints _putchar
  *
  * Return: the return value is 0
  */
+
 int main(void)
 
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+	putchar(_putchar);	
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/test.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+int main(void)
+
+{
+	char  *sh = "main";
+		
+		while (*sh)
+		{
+			_putchar(*sh);
+			sh++;
+		}
+	return (0);
+}
