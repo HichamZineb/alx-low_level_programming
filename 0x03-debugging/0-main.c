eb0545a6f4906ca93969e9757eb2736074f0bcd4
HichamZineb <hichamelamine@hotmail.com>
2023-03-22 11:35:12 -0700

    update3 3-print_remaining_days.c, main.h

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,6 +4,8 @@
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
+
+
 int main(void)
 
 {
@@ -12,5 +14,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
