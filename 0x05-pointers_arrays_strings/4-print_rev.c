b44bc941a8387729e3d58a6046ad53bb977131df
HichamZineb <hichamelamine@hotmail.com>
2023-03-27 11:19:16 -0700

    newfile 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse
+ * @s: string to be reversed
+ *
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int n = 0;
+
+	while (s[n] != '\0')
+	{
+		n++;
+	}
+	for (n -= 1; n >= 0; n--)
+	{
+		_putchar(s[n]);
+	}
+	_putchar('\n');
+}
