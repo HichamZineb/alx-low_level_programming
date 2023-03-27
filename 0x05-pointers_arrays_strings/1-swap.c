2f0caf1c3c089abc86bce3175eaeeb1a13c07b0a
HichamZineb <hichamelamine@hotmail.com>
2023-03-27 07:16:07 -0700

    newfile 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps values of ints
+ * @a: pointer to int
+ * @b: pointer to int
+ *
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+
+{
+	*a = 42;
+	*b = 98;
+}
