3184d2ed5e877baaa758ba9bbeb5affe9f7d9b3b
HichamZineb <hichamelamine@hotmail.com>
2023-03-30 02:26:03 -0700

    newfile 4-rev_arrays.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: string
+ * @n: reversed string
+ *
+ * Return: nothing
+ */
+void reverse_array(int *a, int n)
+
+{
+
+	int i, t;
+
+	for (i = 0; i < n--; i++)
+	{
+		t = a[i];
+		a[i] = a[n];
+		a[n] = t;
+	}
+}
+
