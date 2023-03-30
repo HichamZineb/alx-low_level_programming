8f2926857b84ddfe7b41e3a8f29135821eb5ab49
HichamZineb <hichamelamine@hotmail.com>
2023-03-30 08:14:03 -0700

    newfile 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @str: string to encode
+ *
+ * Return: encoded string
+ */
+char *leet(char *str)
+
+{
+	int i, j;
+	char str1[] = "aAeEoOtTlL";
+	char str2[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; str1[j] != '\0'; j++)
+		{
+			if (str[i] == str1[j])
+			{
+				str[i] = str2[j];
+			}
+		}
+	}
+	return (str);
+}
+		
