d0b64d5383f328541ed4181002370c8464cf32ac
HichamZineb <hichamelamine@hotmail.com>
2023-03-30 08:42:53 -0700

    newfile 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,12 +1,27 @@
 #include "main.h"
 
 /**
- * rot13 - encodes a string using rot13
- * @str: string to be encoded
+ * rot13 - encodes a string into rot13
+ * @str: string to encode
  *
  * Return: encoded string
  */
-char *rot13(char *)
+char *rot13(char *str)
 
 {
+	int i, j;
+	char str1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char str2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; str1[j] != '\0'; j++)
+		{
+			if (str[i] == str1[j])
+			{
+				str[i] = str2[j];
+			}
+		}
+	}
+	return (str);
+}
