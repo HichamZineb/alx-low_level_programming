b5eed14696a470b39ab6d4e0e196b651b1032775
HichamZineb <hichamelamine@hotmail.com>
2023-03-30 04:49:11 -0700

    update2 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,9 +14,18 @@ char *cap_string(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (
-			str[i - 1] == ' '
+			str[i] == 0
+			|| str[i - 1] == ' '
+			|| str[i - 1] == ','
+			|| str[i - 1] == ';'
 			|| str[i - 1] == '.'
-			|| str[i] == 0
+			|| str[i - 1] == '!'
+			|| str[i - 1] == '?'
+			|| str[i - 1] == '"'
+			|| str[i - 1] == '('
+			|| str[i - 1] == ')'
+			|| str[i - 1] == '{'
+			|| str[i - 1] == '}'
 			|| str[i - 1] == '\n'
 			|| str[i - 1] == '\t')
 		{
