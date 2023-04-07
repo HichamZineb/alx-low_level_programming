8eed635caebb89270ec6b2629f2f06c362a120c4
HichamZineb <hichamelamine@hotmail.com>
2023-04-07 07:22:34 -0700

    update2 main.h libmy.a

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,1 +1,17 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * _atoi - convert a string to a double
+ * @s: string
+ *
+ * Return: atoi
+ */
+int _atoi(char *s)
+
+{
+	return atoi(s);
+}
