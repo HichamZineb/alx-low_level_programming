e5a198a4e6265079f5c9bd780177d28918c9a602
HichamZineb <hichamelamine@hotmail.com>
2023-04-13 08:44:40 -0700

    update2 1-string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	else
 		l = i + n + 1;
 
-	s = calloc(sizeof(char), l);
+	s = malloc(l + 1);
 	if (s == NULL)
 		return (NULL);
 	for (k = 0; k < i; k++)
