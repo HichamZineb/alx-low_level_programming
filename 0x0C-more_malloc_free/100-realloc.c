812251f9deaf4d02415eb70f60e4a699e9ca3ee1
HichamZineb <hichamelamine@hotmail.com>
2023-04-13 15:25:51 -0700

    update4 100-realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -28,6 +28,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		p = malloc(new_size);
 		if (p == NULL)
 			return (NULL);
+
+		return (p);
 	}
 	if (new_size > old_size)
 	{
