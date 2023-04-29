649ddac7b68051c08fb1995c89464c1ade63c3cb
HichamZineb <hichamelamine@hotmail.com>
2023-04-29 03:20:58 -0700

    update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * free_listint2 - Frees a list
@@ -12,11 +13,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp = *head;
 
+	if (head == NULL)
+		return;
+
 	while (temp != NULL)
 	{
 		temp = temp->next;
 		free(*head);
 		*head = temp;
 	}
-	*head = NULL;
 }
