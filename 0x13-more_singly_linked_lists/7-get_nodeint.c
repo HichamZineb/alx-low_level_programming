3acea984de94239c92170557e6b6f486ac6db8ca
HichamZineb <hichamelamine@hotmail.com>
2023-05-02 03:42:00 -0700

    update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,11 +12,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
-	if (!head)
-		return (NULL);
-
 	while (i != index)
 	{
+		if (!head)
+			return (NULL);
 		head = head->next;
 		i++;
 	}
