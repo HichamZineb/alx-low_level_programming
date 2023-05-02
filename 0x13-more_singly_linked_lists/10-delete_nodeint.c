be2ef36c6b365567041b50396839d731cee053d4
HichamZineb <hichamelamine@hotmail.com>
2023-05-02 03:34:42 -0700

    update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -20,7 +20,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		(*head) = (*head)->next;
-		free(atindex);
+		free(preindex);
+		return (1);
 	}
 
 	while (i != index - 1)
