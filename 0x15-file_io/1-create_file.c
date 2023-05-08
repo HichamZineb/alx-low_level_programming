9d4b6e3f8aa8a2b79daa7ccfe44c00a9a8be55e1
HichamZineb <hichamelamine@hotmail.com>
2023-05-08 08:51:02 -0700

    update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,6 +16,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+	
 	if (text_content)
 	{
 		while (text_content[i])
@@ -24,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wr = write(file, text_content, i);
 
-	if (!file || !wr)
+	if (file == -1 || wr == -1)
 		return (-1);
 
 	close(file);
