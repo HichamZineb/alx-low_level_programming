0bcf4576d5aa22f284c4f748e23dbf90674fcb7c
HichamZineb <hichamelamine@hotmail.com>
2023-07-07 03:11:19 -0700

    newfile 6-hash_table_delete.c

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table.
+ * @ht: The hash table.
+ *
+ * Return: Nothing.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_table_t *head = ht;
+	hash_node_t *node, *temp;
+	unsigned long int i;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i])
+		{
+			node = ht->array[i];
+			while (node)
+			{
+				temp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = temp;
+			}
+		}
+	}
+	free(head->array);
+	free(head);
+}
