216817655d54062dcb1754f02c769bd84e9d0d5d
HichamZineb <hichamelamine@hotmail.com>
2023-10-11 00:44:33 -0700

    update 103-exponential.c

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -54,7 +54,7 @@ int exponential_search(int *array, size_t size, int value)
 
 	if (array[0] != value)
 	{
-		while (array[i] <= value && i < size - 1)
+		while (array[i] <= value && i < size)
 		{
 			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 			i = i * 2;
