37cd7f743429e0badd8fa31b2337e1ba3d9b73db
HichamZineb <hichamelamine@hotmail.com>
2023-10-11 01:01:08 -0700

    update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -37,6 +37,7 @@ typedef struct skiplist_s
 	struct skiplist_s *express;
 } skiplist_t;
 
+int binarysearch(int *array, size_t left, size_t right, int value);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
